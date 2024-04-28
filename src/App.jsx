15cc06f8b8d56ce96ade570eb331ceb508c6d26c
duynghiadev <duynghia22302@gmail.com>
2024-04-28 16:19:17 +0700

    edit code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import PostList from './components/postList'
-import PostDetail from './components/PostDetial'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import PostDetail from "./components/PostDetial";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<PostList />} />
-        <Route path='/posts/:id' element={<PostDetail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
